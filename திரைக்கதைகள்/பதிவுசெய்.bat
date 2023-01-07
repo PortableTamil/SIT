ff3cd8c30a20f1601517a6796617d61b8b905706
தமிழ்நேரம் <67970539+TamilNeram@users.noreply.github.com>
2023-01-07 12:58:54 +0530

    பயன்பாடுகள் பதிவு
    பயன்பாடுகள் பிதிவு

--- திரைக்கதைகள்/பதிவுசெய்.bat
+++ திரைக்கதைகள்/பதிவுசெய்.bat
@@ -2,6 +2,7 @@
 cd /d %~dp0
 cd ..
 
+Rem REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v திருக்குறள் /t REG_SZ /d "%%பயன்பாடுகள்%%\திரைதிருக்குறள்கள்\திருக்குறள்-0236.bgi\" /Silent /NoLicPrompt /Timer:0" /f
 REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v திருக்குறள் /t REG_SZ /d "%%பயன்பாடுகள்%%\திரைக்கதைகள்\திருக்குறள்.bat" /f
 
 Rem update Environment variable %%பயன்பாடுகள்%% with %cd%
