714e647c7f7b05e88ded33e7518d1be50bcdd88a
தமிழ்நேரம் <67970539+TamilNeram@users.noreply.github.com>
2023-01-07 13:53:47 +0530

    பதிவுசெய்

--- திரைக்கதைகள்/பதிவுசெய்.bat
+++ திரைக்கதைகள்/பதிவுசெய்.bat
@@ -2,8 +2,8 @@
 cd /d %~dp0
 cd ..
 
-Rem REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v திருக்குறள் /t REG_SZ /d "%%பயன்பாடுகள்%%\திரைதிருக்குறள்கள்\திருக்குறள்-0236.bgi\" /Silent /NoLicPrompt /Timer:0" /f
-REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v திருக்குறள் /t REG_SZ /d "%%பயன்பாடுகள்%%\திரைக்கதைகள்\திருக்குறள்.bat" /f
+Rem REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v திருக்குறள் /t REG_EXPAND_SZ /d "%%பயன்பாடுகள்%%\திரைதிருக்குறள்கள்\திருக்குறள்-0236.bgi\" /Silent /NoLicPrompt /Timer:0" /f
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v திருக்குறள் /t REG_EXPAND_SZ /d "%%பயன்பாடுகள்%%\திரைக்கதைகள்\திருக்குறள்.bat" /f
 
 Rem update Environment variable %%பயன்பாடுகள்%% with %cd%
 setx பயன்பாடுகள் %cd% /m
